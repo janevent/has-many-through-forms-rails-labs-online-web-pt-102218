b4425b102e8092682c911d6f3c168803a7ae2aca
Jane Venter <janeanneventer@gmail.com>
2019-04-29 21:35:27 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      #self.categories.find_or_create_by(name: category_attributes[:name])
+      category = Category.find_or_create_by(name: category_attributes[:name]
+      )
+
+      self.categories << category 
+      self.post_categories.build(:category => category)
+    end
+  end
+  #accepts_nested_attributes_for :categories, reject_if: proc { |attributes|# name.blank? }
+  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :users
 
 end
